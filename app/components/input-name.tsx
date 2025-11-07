c0de871499320a2ca0ccb4ef51387d1d53f593f9
Mettel07 <mateushoffmann0707@gmail.com>
2025-11-07 16:04:39 -0300

    refactor: remove unused Link component and clean up input handling in InputName

--- app/components/input-name.tsx
+++ app/components/input-name.tsx
@@ -1,6 +1,5 @@
 'use client';
 
-import Link from 'next/link';
 import { useContextInput } from '../contexts/context-input';
 import { useRouter } from 'next/navigation';
 import { Button } from './ui/button';
@@ -28,18 +27,6 @@ export function InputName() {
           value={context?.name}
           placeholder="Digite seu Nome"
         />
-        {/* <Link
-          href={context?.name === '' ? '' : '/chat-room'}
-          onNavigate={() => {
-            if (context?.name === '') {
-              alert('Preencha um nome!');
-            }
-          }}
-          className="py-4 px-8 rounded-md m-2 bg-linear-to-r to-gray-300 from-teal-300
-          placeholder:text-teal-950 cursor-pointer hover:opacity-80 border border-teal-950"
-        >
-          Iniciar Chat
-        </Link> */}
         <Button
           onClick={() => handleNavigate()}
           className="py-4 px-8 rounded-md m-2 bg-linear-to-r to-gray-300 from-teal-300
